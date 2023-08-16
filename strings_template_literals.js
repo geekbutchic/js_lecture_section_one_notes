126a5a9c41ed9b1d4eb1d581d5ab894ddc415cbb
geekbutchic <71285487+geekbutchic@users.noreply.github.com>
2023-08-16 16:32:47 -0600

    adds notes for template literal strings lecture

--- strings_template_literals.js
+++ strings_template_literals.js
@@ -1,1 +1,21 @@
-const firstName = 'Jonas'
+const firstName = "Sonny";
+const job = "Software Developer";
+const birthYear = 1984;
+const year = 2023;
+
+const sonny =
+  "I'm " + firstName + ", a " + (year - birthYear) + " year old " + job + "!";
+console.log(sonny); // concatenation
+
+const temp_sonny = `I'm ${firstName}, a ${year - birthYear} year old ${job}!`;
+console.log(temp_sonny); // template_literals -> ES6
+
+console.log(`Just a regular string...`);
+
+console.log('String with \n\
+multiple \n\
+lines');
+
+console.log(`String
+multiple 
+lines`); // press return
