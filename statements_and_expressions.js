8b5e190e0c87963122a5c957a84b756ebd997156
geekbutchic <71285487+geekbutchic@users.noreply.github.com>
2023-08-26 14:50:32 -0600

    compose file for statement and expression lecture

---
+++ statements_and_expressions.js
@@ -0,0 +1,2 @@
+//In JavaScript, an expression is a piece of code that produces a value. An expression can be a combination of variables, constants, operators, and function calls that are evaluated to yield a result. 
+
