3923b67a95c839c072348697241e98bdd5f5c5c6
geekbutchic <71285487+geekbutchic@users.noreply.github.com>
2023-08-30 17:53:06 -0600

    adds notes on JS changes

---
+++ js_es5_es6_esnext.js
@@ -0,0 +1,12 @@
+// 1995 Js 1st version called Mocha
+// Standardize the language, ECMA the first official standard for JS.
+// ES6/ES2015 was released : the biggest update to the language ever!
+// Annual release cycle
+
+/* 
+Backwards compatibility : don't break the web
+ES1 1997 -> 2020
+* Old features are never removed.
+* Not really new versions, just incremental updates (releases)
+Websites keep working forever!
+*/
