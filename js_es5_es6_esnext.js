78e1ba782cf325424b90d5133a9da0cd3022a779
geekbutchic <71285487+geekbutchic@users.noreply.github.com>
2023-08-30 18:03:36 -0600

    adds lecture notes

--- js_es5_es6_esnext.js
+++ js_es5_es6_esnext.js
@@ -9,4 +9,25 @@ ES1 1997 -> 2020
 * Old features are never removed.
 * Not really new versions, just incremental updates (releases)
 Websites keep working forever!
+
+JS is not forward compatible
 */
+
+/* 
+How to use modern JS today?
+
+During Development : Simply use the latest Google Chrome!
+
+During Production : Use Babel to transpile and polyfill your code (converting back to ES5 to ensure browser compatibility for all users.)
+*/
+
+/* 
+ES5 -> Fully supported in all browsers (down to IE 9 from 2011)
+
+ES6/ES2015 -> ES2020 
+
+* ES6+ : Well supported in all modern browsers.
+* No support in older browsers.
+* Can use most features in production with transpiling and polyfilling.
+*/
+
